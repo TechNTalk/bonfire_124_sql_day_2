32bcd9d9f310ac4c02ad4a60ae2a44aa2c43ee3f
Joshua L <138939574+TechNTalk@users.noreply.github.com>
2023-08-09 10:11:12 -0400

    update

--- movie_theater_insert.sql
+++ movie_theater_insert.sql
@@ -1,53 +1,53 @@
--- INSERT INTO customer(
---     customer_id,
---     first_name,
---     last_name,
---     address,
---     billing_info
--- )VALUES(
---     01,
---     'Joshua',
---     'Lewis',
---     '123 Main Street, Westerville, OH 43081',
---     '4242-4242-4242-4242 623 08/07'
--- );
+INSERT INTO customer(
+    customer_id,
+    first_name,
+    last_name,
+    address,
+    billing_info
+)VALUES(
+    01,
+    'Joshua',
+    'Lewis',
+    '123 Main Street, Westerville, OH 43081',
+    '4242-4242-4242-4242 623 08/07'
+);
 
--- Insert statement for inventory data
--- INSERT INTO inventory(
---     item_id,
---     upc,
---     product_amount
--- )VALUES(
---     01,
---     01,
---     15.00
--- );
+Insert statement for inventory data
+INSERT INTO inventory(
+    item_id,
+    upc,
+    product_amount
+)VALUES(
+    01,
+    01,
+    15.00
+);
 
--- -- Insert statement for the inventory data
--- INSERT INTO in_house_restaurant(
---     receipt_id,
---     customer_id,
---     sub_total,
---     total_cost,
---     upc
--- )VALUES(
---     01,
---     01,
---     20.00,
---     23.50,
---     01
--- );
+-- Insert statement for the inventory data
+INSERT INTO in_house_restaurant(
+    receipt_id,
+    customer_id,
+    sub_total,
+    total_cost,
+    upc
+)VALUES(
+    01,
+    01,
+    20.00,
+    23.50,
+    01
+);
 
--- -- Insert statement for the product data
--- INSERT INTO product(
---     upc,
---     quantity,
---     product_name
--- )VALUES(
---     01,
---     2,
---     'Popcorn'
--- );
+-- Insert statement for the product data
+INSERT INTO product(
+    upc,
+    quantity,
+    product_name
+)VALUES(
+    01,
+    2,
+    'Popcorn'
+);
 
 -- Insert statement for order data
 INSERT INTO concession(
